d8461a0ab858a22c22217114ebdb3a41162b09f4
chthjo <78281023+chthjo@users.noreply.github.com>
2021-03-11 11:45:55 -0800

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, Form, SelectField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+from app.models import Calendar
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+            'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please enter a different username.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class requestWeek(FlaskForm):
+    week  = IntegerField('Enter a week number to request off:', validators=[DataRequired()])
+    user = StringField('Enter your name to reserve:', validators=[DataRequired()])
+    submit = SubmitField('Submit Request')
+
+    def validate_week(self, week):
+        wcheck = Calendar.query.filter_by(week=week.data).first()
+        if wcheck is not None:
+            raise ValidationError('This week is already being requested by a user')
+
+class requestWeek1(FlaskForm):
+    week  = IntegerField('Enter a week number to request off:', validators=[DataRequired()])
+    submit = SubmitField('Submit Request')
+
+    def validate_week1(self, week):
+        wcheck = Calendar.query.filter_by(week=week.data).first()
+        if wcheck is not None:
+            raise ValidationError('This week is already being requested by a user')
+class checkWeek(FlaskForm):
+    week  = IntegerField('Enter a week number to check availability:')
+    user  = StringField('Enter a user to see request history')
+    submit = SubmitField('Submit Request')
+
---
+++ app/models.py
@@ -0,0 +1,46 @@
+from app import login
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Calendar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    week = db.Column(db.Integer, unique=True)
+    user = db.Column(db.String(20), unique=False)
+
+    def __repr__(self):
+        return '<Calendar week: {}, User requested: {}>'.format(self.week, self.user)
+
+    def show_requests():
+        return '<Calendar Week {}: User: {}>'.format(self.week, self.user)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
